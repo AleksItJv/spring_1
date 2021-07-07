f63e8fb41f8f78f6d2f264dfdf7ec7c63d8537c2
AleksItJv <aleks.it.jv@gmail.com>
2021-07-07 19:04:28 +0300

    Master-1: commit

--- src/main/java/impl/audiosystem/Sony.java
+++ src/main/java/impl/audiosystem/Sony.java
@@ -3,20 +3,18 @@ package impl.audiosystem;
 import impl.songs.Song;
 import interfaces.Audiosystem;
 import lombok.Data;
-
-import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 @Data
 public class Sony implements Audiosystem {
 
     private String name = "Sony Music";
-    private Set<Song> playlist;
+    private Map<String, Song> playlist;
 
     public Sony() {
     }
 
-    public Sony(Set<Song> playlist) {
+    public Sony(Map<String, Song> playlist) {
         this.playlist = playlist;
     }
 
